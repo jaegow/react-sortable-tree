21a04dbaab1c513e17173c6ad3fb446dd4e32f72
Merynek <001little001>
2021-01-19 14:10:24 +0100

    dragging item in another tree (warning)

--- src/react-sortable-tree.js
+++ src/react-sortable-tree.js
@@ -239,7 +239,7 @@ class ReactSortableTree extends Component {
     // it means that the drag was canceled or the dragSource dropped
     // elsewhere, and we should reset the state of this tree
     if (!monitor.isDragging() && this.state.draggingTreeData) {
-      this.endDrag();
+      setTimeout(() => {this.endDrag()});
     }
   }
 
